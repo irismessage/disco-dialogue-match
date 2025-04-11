461446586c959dbdb802f79a5c1a1c0b19176e55
imcb <irismessage@protonmail.com>
2025-04-11 22:08:18 +0100

    print matches?

--- diff.py
+++ diff.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 import difflib
+from difflib import Match
 from pathlib import Path
 
 
 # could maybe parallelise this by combining blocks?
+def print_matching_blocks(a, b):
+    matcher = difflib.SequenceMatcher(a=a, b=b)
+    print(matcher.get_matching_blocks())
+
+
+def load_matching_blocks() -> list[Match]:
+    fp = Path("matches.txt")
+    matches = eval(fp.read_text())
+    return matches
+
 
 def main():
     a_fp = Path("dialogue-process/done.txt")
@@ -11,8 +22,11 @@ def main():
     a = a_fp.read_text()
     b = b_fp.read_text()
 
-    matcher = difflib.SequenceMatcher(a=a, b=b)
-    print(matcher.get_matching_blocks())
+    # print_matching_blocks(a, b)
+
+    matches = load_matching_blocks()
+    for m in matches:
+        print(a[m.a:m.a + m.size])
 
 
 if __name__ == "__main__":
