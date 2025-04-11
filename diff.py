0be940dde1c348bd7757912f426382ca6eb33fae
imcb <irismessage@protonmail.com>
2025-04-11 21:52:18 +0100

    diff script

---
+++ diff.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import difflib
+from pathlib import Path
+
+
+def main():
+    a_fp = Path("dialogue-process/done.txt")
+    b_fp = Path("lyrics-process/done.txt")
+    a = a_fp.read_text()
+    b = b_fp.read_text()
+
+    matcher = difflib.SequenceMatcher(a=a, b=b)
+    print(matcher.get_matching_blocks())
+
+
+if __name__ == "__main__":
+    main()
--- lyrics_from_html.py
+++ lyrics_from_html.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from sys import argv
+
 from bs4 import BeautifulSoup
 
 
