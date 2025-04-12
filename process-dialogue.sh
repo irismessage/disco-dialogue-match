b9c322ab59079852307b484083bd506c5b55a03a
imcb <irismessage@protonmail.com>
2025-04-12 12:04:50 +0100

    more scripting

--- process-dialogue.sh
+++ process-dialogue.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+set -eux
+
 dir='dialogue-process'
-tr '[:upper:]' '[:lower:]' < 'dialoguetext.txt' > "${dir}/lower.txt"
-tr  --delete '[:punct:]' < "${dir}/lower.txt" > "${dir}/no-punctuation.txt"
-tr --squeeze-repeats '[:space:]' < "${dir}/no-punctuation.txt" > "${dir}/squeeze-space.txt"
-tr '[:space:]' ' ' < "${dir}/squeeze-space.txt" > "${dir}/regular-space.txt"
-cp "${dir}/regular-space.txt" "${dir}/done.txt"
+mkdir -p "${dir}"
+
+tr '[:upper:]' '[:lower:]' < 'dialoguetext.txt' > "${dir}/1-lower.txt"
+tr  --delete '[:punct:]' < "${dir}/1-lower.txt" > "${dir}/2-no-punctuation.txt"
+tr --squeeze-repeats '[:space:]' < "${dir}/2-no-punctuation.txt" > "${dir}/3-squeeze-space.txt"
+tr '[:space:]' ' ' < "${dir}/3-squeeze-space.txt" > "${dir}/4-regular-space.txt"
+
+cp "${dir}/4-regular-space.txt" "${dir}/done.txt"
