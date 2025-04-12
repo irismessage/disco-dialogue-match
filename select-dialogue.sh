fb2c083b701f53cbbc580f65aaed60e5c08c126e
imcb <irismessage@protonmail.com>
2025-04-12 15:30:14 +0100

    edit script

--- select-dialogue.sh
+++ select-dialogue.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -eux
 
-sqlite3 'discobase12-17-2021-4-18-51-PM.db' 'SELECT dialoguetext FROM dentries;' > dialoguetext.txt
+sqlite3 \
+    'discobase12-17-2021-4-18-51-PM.db' \
+    'SELECT dialoguetext FROM dentries;'
