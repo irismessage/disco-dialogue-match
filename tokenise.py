a204fc420fd0ea90a8daab25f6d978ef6b046af8
imcb <irismessage@protonmail.com>
2025-04-12 11:06:35 +0100

    tokeniser

---
+++ tokenise.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import json
+from pathlib import Path
+from sys import argv
+from typing import Union
+
+PathLike = Union[str, Path]
+
+TABLE_FILE = "token_mapping.json"
+
+
+class TokenTable:
+    def __init__(self):
+        self.word_to_token: dict[str, int] = {}
+        self.token_to_word: dict[int, str] = {}
+        self.tokenised_words: set[str] = set()
+        self.next_token = 1
+
+    @classmethod
+    def load(cls, fp: PathLike):
+        table = cls()
+        with open(fp) as f:
+            table.word_to_token = json.load(f)
+        table.token_to_word = {v: k for k, v in table.word_to_token.items()}
+        table.tokenised_words = set(table.word_to_token.keys())
+        table.next_token = len(table.tokenised_words)
+
+    def save(self, fp: PathLike):
+        with open(fp, "w") as f:
+            json.dump(self.word_to_token, f)
+
+    def tokenise_text(self, text: str) -> list[int]:
+        words = text.split()
+        tokens = []
+        for w in words:
+            if w not in self.tokenised_words:
+                self.word_to_token[w] = self.next_token
+                self.token_to_word[self.next_token] = w
+                self.next_token += 1
+                self.tokenised_words.add(w)
+            tokens.append(self.word_to_token[w])
+
+        return tokens
+
+    def detokenise_text(self, tokens: list[int]) -> str:
+        return " ".join(self.token_to_word[t] for t in tokens)
+
+
+def main():
+    try:
+        table = TokenTable.load(TABLE_FILE)
+    except FileNotFoundError:
+        table = TokenTable()
+
+    with open(argv[1]) as f:
+        text = f.read()
+
+    tokens = table.tokenise_text(text)
+    table.save()
+
+    print(tokens)
+
+
+if __name__ == "__main__":
+    main()
