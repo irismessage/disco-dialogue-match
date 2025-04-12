8fcff33cae6ea90d8355b672ac29171de0d2e2f2
imcb <irismessage@protonmail.com>
2025-04-12 12:08:57 +0100

    more scripting

--- process-lyrics.sh
+++ process-lyrics.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -eux
 
+in_file='all-lyrics.txt'
 dir='lyrics-process'
 mkdir -p "${dir}"
 
-tr '[:upper:]' '[:lower:]' < 'all-lyrics.txt' > "${dir}/1-lower.txt"
+tr '[:upper:]' '[:lower:]' < "${in_file}" > "${dir}/1-lower.txt"
 tr  --delete '[:punct:]' < "${dir}/1-lower.txt" > "${dir}/2-no-punctuation.txt"
-tr --squeeze-repeats '[:space:]' < "${dir}/2-no-punctuation.txt" > "${dir}/3-squeeze-space.txt"
-tr '[:space:]' ' ' < "${dir}/3-squeeze-space.txt" > "${dir}/4-regular-space.txt"
+tr '[:space:]' ' ' < "${dir}/2-no-punctuation.txt" > "${dir}/3-regular-space.txt"
+tr --squeeze-repeats '[:space:]' < "${dir}/3-regular-space.txt" > "${dir}/4-squeeze-space.txt"
 
-cp "${dir}/4-regular-space.txt" "${dir}/done.txt"
+cp "${dir}/4-squeeze-space.txt" "${dir}/done.txt"
